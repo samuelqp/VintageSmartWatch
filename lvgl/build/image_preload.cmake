ec415bc78a5826a95e29ddf0e98a9a6477f0604e
Samuel Quinteiro Perez <119834862+samuelqp@users.noreply.github.com>
2024-04-21 00:19:13 -0400

    Add lvgl project for 64x32 display

---
+++ lvgl/build/image_preload.cmake
@@ -0,0 +1,10 @@
+# Generated file that can be used to preload variant images
+set(BOARD "nrf52dk_nrf52832" CACHE INTERNAL "NCS child image controlled")
+set(BOARD_ROOT "c:/nordic/displaytests/lvgl;C:/ncs/v2.4.1/nrf;C:/ncs/v2.4.1/zephyr" CACHE INTERNAL "NCS child image controlled")
+set(CACHED_CONF_FILE "c:/nordic/displaytests/lvgl/prj.conf" CACHE INTERNAL "NCS child image controlled")
+set(CONF_FILE_BUILD_TYPE "" CACHE INTERNAL "NCS child image controlled")
+set(DTC_OVERLAY_FILE "c:/nordic/displaytests/lvgl/nrf52dk_nrf52832.overlay" CACHE INTERNAL "NCS child image controlled")
+set(DTC_OVERLAY_FILE "c:/nordic/displaytests/lvgl/nrf52dk_nrf52832.overlay" CACHE INTERNAL "NCS child image controlled")
+set(NCS_TOOLCHAIN_VERSION "NONE" CACHE INTERNAL "NCS child image controlled")
+set(SHIELD "ssd1306_128x64" CACHE INTERNAL "NCS child image controlled")
+set(WEST_PYTHON "C:/ncs/toolchains/31f4403e35/opt/bin/python.exe" CACHE INTERNAL "NCS child image controlled")
